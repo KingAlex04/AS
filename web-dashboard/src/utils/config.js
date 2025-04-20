d11ec81c6d2563649b496ec1e53882fa436b93d8
Laxmi Sah <laxmisah32145@gmail.com>
2025-04-20 23:39:31 +0545

    Added Netlify config and updated API URL

--- web-dashboard/src/utils/config.js
+++ web-dashboard/src/utils/config.js
@@ -1,5 +1,5 @@
 // API URL for backend
-export const API_URL = 'http://localhost:5000';
+export const API_URL = 'https://hrm-tracker-api.onrender.com';
 
 // App name
 export const APP_NAME = 'HRM Location Tracker'; 
